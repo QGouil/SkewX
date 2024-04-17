680a6c809a5c78a8772ab1bb14fe88b195ded028
Edward Yang <yang.e@wehi.edu.au>
2024-04-17 11:04:03 +1000

    adapt samplesheet validation

--- bin/check_samplesheet.py
+++ bin/check_samplesheet.py
@@ -26,21 +26,23 @@ class RowChecker:
 
     def __init__(
         self,
-        sample_col="sample",
-        first_col="raw_dir",
+        individual_col="individual",
+        tissue_col="tissue",
+        modbam_col="modbam_5mCG_dir",
         **kwargs,
     ):
         """
         Initialize the row checker with the expected column names.
 
         Args:
-            sample_col (str): The name of the column that contains the sample name
-                (default "sample").
-            first_col (str): The name of the column that contains the fast5/pod5 directory path (default "raw_dir").
+            individual_col (str): The name of the column that contains the individual name
+                (default "individual").
+            tissue_col (str): The name of the column that specifies the colon-delimited list of tissues of the sample.
         """
         super().__init__(**kwargs)
-        self._sample_col = sample_col
-        self._first_col = first_col
+        self._individual_col = individual_col
+        self._tissue_col = tissue_col
+        self._modbam_col = modbam_col
         self._seen = set()
         self.modified = []
 
@@ -53,39 +55,46 @@ class RowChecker:
                 (values).
 
         """
+        self._validate_individual(row)
         self._validate_sample(row)
-        self._validate_first(row)
-        self._seen.add((row[self._sample_col], row[self._first_col]))
+        self._validate_modbamdir(row)
+        self._seen.add((row[self._individual_col], row[self._tissue_col]))
         self.modified.append(row)
 
-    def _validate_sample(self, row):
-        """Assert that the sample name exists and convert spaces to underscores."""
-        if len(row[self._sample_col]) <= 0:
-            raise AssertionError("Sample input is required.")
-        # Sanitize samples slightly.
-        row[self._sample_col] = row[self._sample_col].replace(" ", "_")
+    def _validate_individual(self, row):
+        """Assert that the individual name exists and convert spaces to underscores."""
+        if len(row[self._individual_col]) <= 0:
+            raise AssertionError("Individual input is required.")
+        # Sanitize individuals slightly.
+        row[self._individual_col] = row[self._individual_col].replace(" ", "_")
 
-    def _validate_first(self, row):
-        """Assert that the raw_dir entry is non-empty and has the right format."""
-        if len(row[self._first_col]) <= 0:
-            raise AssertionError("raw file directory is required.")
+    def _validate_sample(self, row):
+        """Assert that the tissue names exists."""
+        if len(row[self._tissue_col]) <= 0:
+            raise AssertionError("List of tissues is required.")
+       # self._validate_fastq_format(row[self._first_col])
+    
+    def _validate_modbamdir(self, row):
+        """Assert that the column with modified bam files exists."""
+        if len(row[self._modbam_col]) <= 0:
+            raise AssertionError("Directory of modified BAMs is required.")
        # self._validate_fastq_format(row[self._first_col])
 
-    def validate_unique_samples(self):
+    def validate_unique_individuals(self):
         """
-        Assert that the combination of sample name and FASTQ filename is unique.
+        Assert that the combination of individual name and modified BAM directory is unique.
 
-        In addition to the validation, also rename all samples to have a suffix of _T{n}, where n is the
-        number of times the same sample exist, but with different FAST5 folders, e.g., multiple runs per experiment.
+        In addition to the validation, also rename all individuals to have a suffix of _T{n}, where n is the
+        number of times the same individual exist, but with different folders, e.g., multiple runs per experiment.
 
         """
         if len(self._seen) != len(self.modified):
-            raise AssertionError("The pair of sample name and raw file directory must be unique.")
+            raise AssertionError("The pair of individual name and modified BAM directory must be unique.")
         seen = Counter()
         for row in self.modified:
-            sample = row[self._sample_col]
-            seen[sample] += 1
-            row[self._sample_col] = f"{sample}_T{seen[sample]}"
+            individual = row[self._individual_col]
+            seen[individual] += 1
+            row[self._individual_col] = f"{individual}_T{seen[individual]}"
 
 
 def read_head(handle, num_lines=10):
@@ -147,7 +156,7 @@ def check_samplesheet(file_in, file_out):
         https://raw.githubusercontent.com/nf-core/test-datasets/viralrecon/samplesheet/samplesheet_test_illumina_amplicon.csv
 
     """
-    required_columns = {"sample", "raw_dir"}
+    required_columns = {"individual", "tissue", "modbam_5mCG_dir"}
     # See https://docs.python.org/3.9/library/csv.html#id3 to read up on `newline=""`.
     with file_in.open(newline="") as in_handle:
         reader = csv.DictReader(in_handle, dialect=sniff_format(in_handle))
@@ -164,7 +173,7 @@ def check_samplesheet(file_in, file_out):
             except AssertionError as error:
                 logger.critical(f"{str(error)} On line {i + 2}.")
                 sys.exit(1)
-        checker.validate_unique_samples()
+        checker.validate_unique_individuals()
     header = list(reader.fieldnames)
     # See https://docs.python.org/3.9/library/csv.html#id3 to read up on `newline=""`.
     with file_out.open(mode="w", newline="") as out_handle:
